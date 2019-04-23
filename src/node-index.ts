e146a7d7aff5f90d7982a03761150cce5e39028b
Genna Wingert <wingertge@gmail.com>
2019-04-23 10:58:18 +0200

    a

--- src/node-index.ts
+++ src/node-index.ts
@@ -3,6 +3,6 @@ dotenv.config() // This needs to be before all other imports so process.env is s
 
 import {app, server} from "./common/server"
 
-app.listen({port: process.env.API_PORT || 4000}, () => {
-    console.log(`🚀  Server ready at ${process.env.ENDPOINT_URL}:${process.env.API_PORT || 4000}${server.graphqlPath}`)
+app.listen({port: process.env.PORT || 4000}, () => {
+    console.log(`🚀  Server ready at ${process.env.ENDPOINT_URL}:${process.env.PORT || 4000}${server.graphqlPath}`)
 })
