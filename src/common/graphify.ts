7585581bca17528b176a3fe6a8457c1f72eed1bd
Genna Wingert <wingertge@gmail.com>
2019-04-24 14:09:34 +0200

    a

--- src/common/graphify.ts
+++ src/common/graphify.ts
@@ -1,3 +1,8 @@
-export const graphify = (obj: any) => JSON.parse(JSON.stringify(obj))
+export const graphify = (obj: any) => {
+    const newObj = JSON.parse(JSON.stringify(obj))
+    newObj.id = newObj._id
+    delete newObj._id
+    return newObj
+}
 
 export default graphify
