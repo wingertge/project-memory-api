9e6e8bde129ac44b0d648dcda7797a09e3a9ccff
Genna Wingert <wingertge@gmail.com>
2019-05-11 12:53:38 +0200

    Remove old button left over from old upload component

--- src/common/cloudFunctionFix.ts
+++ src/common/cloudFunctionFix.ts
@@ -5,14 +5,10 @@ import toStream from "buffer-to-stream"
 const logger = makeLogger("custom-middleware")
 
 export const cloudFunctionFix = (req: Request & {rawBody?: Buffer}, res: Response, next: NextFunction) => {
-    if(req.is("multipart/form-data") && process.env.IS_SERVERLESS) {
+    if(process.env.IS_SERVERLESS && req.is("multipart/form-data")) {
         logger.debug("Overriding pipe")
         const bodyStream = toStream(req.rawBody)
         req.pipe = bodyStream.pipe.bind(bodyStream)
     }
     next()
-/*    if (!req.is("multipart/form-data") || !req.rawBody) return next()
-
-    logger.debug(req.rawBody.toString())
-    req.body = req.rawBody*/
 }
